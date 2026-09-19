94961e023b020e193925f7c42292add33a2ff412
Alipta Ghosh <aliptaghosh7@gmail.com>
2026-09-19 12:34:14 +0530

    NEXT_PUBLIC_API_URL changed to NEXT_API_URL

--- api/axios/axios.ts
+++ api/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const resolveApiBaseUrl = () => {
-  const configuredUrl = process.env.NEXT_PUBLIC_API_URL ?? process.env.BASE_URL ?? "http://localhost:4000/api/v1";
+  const configuredUrl = process.env.NEXT_API_URL ?? process.env.BASE_URL ?? "http://localhost:4000/api/v1";
   return configuredUrl.replace(/\/$/, "");
 };
 
